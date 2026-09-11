7832871f710c58e5c47e978218d59e3fa2537338
Stanley Lam <87041487+Stanley-Lam@users.noreply.github.com>
2026-09-11 10:45:25 -0700

    Add files via upload

---
+++ Income_Cleaning.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+income_data <- read.csv("C:/Users/stanl/OneDrive/Documents/OneNote Notebooks/Data Science/4401 - Data Science Process and Ethics/Project 1/IOWA_M~1/IOWA_M~1.csv")
+
+income_clean <- income_data |>
+  filter(geography_type == "County") |>
+  select(geography_name, data_collection_period, is_current_period, median_household_income, margin_of_error, change, change_rate)
+
+
+
+
+find <- income_clean |>
+  distinct(geography_name) #double checking all counties exist
+  
