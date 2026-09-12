b351773a7d65b7505d343b034eb1986a815d68e4
Stanley Lam <87041487+Stanley-Lam@users.noreply.github.com>
2026-09-12 16:18:43 -0700

    added 1 comment
    Updated the file path for the income data CSV.

--- Income_Cleaning.R
+++ Income_Cleaning.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-income_data <- read.csv("C:/Users/stanl/OneDrive/Documents/OneNote Notebooks/Data Science/4401 - Data Science Process and Ethics/Project 1/IOWA_M~1/IOWA_M~1.csv")
+income_data <- read.csv("C:/Users/stanl/OneDrive/Documents/OneNote Notebooks/Data Science/4401 - Data Science Process and Ethics/Project 1/IOWA_M~1/IOWA_M~1.csv") #change file path
 
 income_clean <- income_data |>
   filter(geography_type == "County") |>
